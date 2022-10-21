9c0410cf88e1445ad0492c7365831597de9fcb8e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-21 15:29:17 +0200

    Initial Commit

---
+++ MY-Firebase/MY_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_FirebaseApp.swift
+//  MY-Firebase
+//
+//  Created by Zaid Neurothrone on 2022-10-21.
+//
+
+import SwiftUI
+
+@main
+struct MY_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
